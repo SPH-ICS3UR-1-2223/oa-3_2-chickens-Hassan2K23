8b354c2d93dcc4a42e3053038bb7208842327809
Hassan2K23 <113380661+Hassan2K23@users.noreply.github.com>
2022-11-23 13:52:52 -0500

    Chickens Completed

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,17 @@
 public class Chickens01 {
     public static void main(String[] args) {
-        //Put your code here
         
-        System.out.println(totalEggs);
-    }   
-}
+        
+       
+        int eggsPerChicken = 0;
+        int totalChickens = 3;
+        int x = 5;
+        int y = x + 5;
+        System.out.println(totalChickens);
+        System.out.println(eggsPerChicken);
+        System.out.println(y);
+        System.out.println(++y);
+        System.out.println(x);
+        System.out.println(++x);
+        }   
+        }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,10 +1,31 @@
 public class Chickens02 {
     public static void main(String[] args) {
-        //Put your code here
-
+        
+        int eggsPerChicken = 4;
+        int totalEggs = 0;
+        int chickenCount = 8;
+        // Monday
+        totalEggs = eggsPerChicken * chickenCount;
+        // Tuesday
+        totalEggs += eggsPerChicken * chickenCount;
+        // Wednesday
+        chickenCount /= 2;
+        totalEggs += eggsPerChicken * chickenCount;
+        double dailyAverage = 0;
+        double monthlyProfit = 0;
+        double monthlyAverage = dailyAverage * 30;
+        double eggCount = 100;
+        // MONDAY
+        eggCount += 100;
+        // TUESDAY
+        eggCount += 121;
+        // WEDNESDAY
+        eggCount =+ 117;
+        dailyAverage = eggCount / 3;
+        monthlyAverage = dailyAverage * 30;
+        monthlyProfit = monthlyAverage * 0.18;
         System.out.println("Daily Average:   " +dailyAverage);
         System.out.println("Monthly Average: " +monthlyAverage);
         System.out.println("Monthly Profit:  $" +monthlyProfit);
-    }
-    
-}
+        }
+        }
